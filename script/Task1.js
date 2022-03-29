02405e3c2f85f5e7ae00f9858a9fe06af6739926
Ivan Ushachov <ushachovg324@gmail.com>
2022-03-29 20:20:50 +0300

    q

---
+++ script/Task1.js
@@ -0,0 +1,17 @@
+// let numOrStr = prompt('input number or string');
+// console.log(numOrStr)
+//
+// let shortCode ;
+//
+//
+//
+//
+// if(numOrStr === null) {
+//     console.log('вы отменили')
+// } else {
+//     shortCode = ( numOrStr.trim() === '' ) ? 'Empty String' : ( isNaN( +numOrStr ) ) ? 'number is Ba_NaN' : 'OK!';
+//     console.log(shortCode);
+// }
+
+
+
